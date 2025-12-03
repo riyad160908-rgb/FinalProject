9f3d4d931f17ea2f624e0817bcc90478fe2683d5
riyad160908-rgb <riyad160908@gmail.com>
2025-12-03 18:37:10 -0500

    Add Address class

---
+++ src/main/java/org/riyad/Address.java
@@ -0,0 +1,9 @@
+package org.riyad;
+
+public class Address {
+    private int streetNo;
+    private String street;
+    private String city;
+    private Province province;
+    private String postalCode;
+}
