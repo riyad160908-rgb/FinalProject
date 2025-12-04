e7602fd666be052bed1dfa341684d671c69c64e2
riyad160908-rgb <riyad160908@gmail.com>
2025-12-03 22:31:30 -0500

    lombok

--- src/main/java/org/riyad/Address.java
+++ src/main/java/org/riyad/Address.java
@@ -1,5 +1,14 @@
 package org.riyad;
 
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@EqualsAndHashCode
 public class Address {
     private int streetNo;
     private String street;
