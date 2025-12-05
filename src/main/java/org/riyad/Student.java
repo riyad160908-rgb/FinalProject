32c9a070a93e35e20e2ac87f8a9c5c42fa612bf9
riyad160908-rgb <riyad160908@gmail.com>
2025-12-05 08:44:40 -0500

    fields

--- src/main/java/org/riyad/Student.java
+++ src/main/java/org/riyad/Student.java
@@ -3,4 +3,10 @@ package org.riyad;
 public class Student {
     private String studentId;
     private String studentName;
+    private Gender gender;
+    private Address address;
+    private Department department;
+    private ArrayList<Course> registeredCourses;
+
+    private static int nextId = 1;
 }
