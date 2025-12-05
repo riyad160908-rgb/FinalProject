5c975fd22da4e5aa3f258bd10e60460300612e6c
riyad160908-rgb <riyad160908@gmail.com>
2025-12-05 10:46:19 -0500

    fields

--- src/main/java/org/riyad/Assignment.java
+++ src/main/java/org/riyad/Assignment.java
@@ -1,4 +1,12 @@
 package org.riyad;
 
+import java.util.ArrayList;
+
 public class Assignment {
+    private String assignmentId;
+    private String assignmentName;
+    private double weight;
+    private ArrayList<Integer> scores;
+
+    private static int nextId = 1;
 }
