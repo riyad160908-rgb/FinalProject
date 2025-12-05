d1e5b56a97b56a8a9ea96875ec4ac3ce1910de16
riyad160908-rgb <riyad160908@gmail.com>
2025-12-05 10:41:42 -0500

    fields

--- src/main/java/org/riyad/Course.java
+++ src/main/java/org/riyad/Course.java
@@ -1,4 +1,15 @@
 package org.riyad;
 
+import java.util.ArrayList;
+
 public class Course {
+    private String courseId;
+    private String courseName;
+    private double credits;
+    private Department department;
+    private ArrayList<Assignment> assignments;
+    private ArrayList<Student> registeredStudents;
+
+    private static int nextId = 1;
+
 }
