351ba57030132b75ae8db6acb52ae6b8eefb6a18
riyad160908-rgb <riyad160908@gmail.com>
2025-12-08 17:36:23 -0500

    calcAssignmentAvg method

--- src/main/java/org/riyad/Assignment.java
+++ src/main/java/org/riyad/Assignment.java
@@ -10,7 +10,33 @@ public class Assignment {
     private String assignmentId;
     private String assignmentName;
     private double weight;
-    private ArrayList<Integer> scores;
+    private ArrayList<Integer> scores = new ArrayList<>();
 
     private static int nextId = 1;
+
+
+    public void calcAssignmetnAvg() {
+        if (scores.isEmpty()) {
+            System.out.println("No scores for " + assignmentName);
+            return;
+        }
+
+        int sum = 0;
+        int count = 0;
+
+        for (Integer score : scores) {
+            if (score != null) {
+                sum += score;
+                count++;
+            }
+        }
+
+        if (count == 0) {
+            System.out.println("No valid scores for " + assignmentName);
+            return;
+        }
+
+        double avg =  (double) sum / count;
+        System.out.println("Average for " + assignmentName + " is " + avg);
+    }
 }
