64a07347ce7a7efbee2098b1985712f83691ed15
riyad160908-rgb <riyad160908@gmail.com>
2025-12-14 10:35:35 -0500

    toTitleCase method

--- src/main/java/org/riyad/Util.java
+++ src/main/java/org/riyad/Util.java
@@ -1,4 +1,21 @@
 package org.riyad;
 
 public class Util {
+    public static String toTitleCase(String str) {
+        if (str == null || str.isBlank()) {
+            return str;
+        }
+        String[] words = str.split(" ");
+        String result = "";
+
+        for (int i = 0; i < words.length; i++) {
+            String w = words[i];
+            result += w.substring(0, 1).toUpperCase() + w.substring(1).toLowerCase();
+
+            if (i < words.length - 1) {
+                result += " ";
+            }
+        }
+        return result;
+    }
 }
