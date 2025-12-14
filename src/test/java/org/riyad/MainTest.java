a87219828052e10fc524329ae4df68b09383fa6b
riyad160908-rgb <riyad160908@gmail.com>
2025-12-14 13:29:19 -0500

    AddressTest

--- src/test/java/org/riyad/MainTest.java
+++ src/test/java/org/riyad/MainTest.java
@@ -1,14 +1,32 @@
 package org.riyad;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-public class MainTest {
+ class AddressTest {
 
     @Test
-    @DisplayName("")
-    void isPostalCodeValid() {
-
+    @DisplayName("isPostalCodeValid: null -> false")
+    void isPostalCodeValid1() {
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid(null);
+        Assertions.assertEquals(expected, actual);
     }
 
+    @Test
+    @DisplayName("isPostalCodeValid: \"\" -> false")
+    void isPostalCodeValid2() {
+        boolean expected = false;
+        boolean actual = Address.isPostalCodeValid("");
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    @DisplayName("isPostalCodeValid: A3B4C8 -> false")
+    void isPostalCodeValid3() {
+        boolean expected = true;
+        boolean actual = Address.isPostalCodeValid("A3B4C8");
+        Assertions.assertEquals(expected, actual);
+    }
 }
